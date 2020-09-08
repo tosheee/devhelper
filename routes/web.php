a2ff110d336141ee8fcc70968dd279f2df7799b0
Todor Chakarov <tosheee@abv.bg>
2020-09-08 11:52:25 +0300

    Change routes

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', function () { return view('home'); })->middleware('auth');
+
+Route::get('/home', 'HomeController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//Route::get('/', function () {
+//    if(Auth::check()){
+  //      return Redirect::to('home');
+   // }
+   // return Route::get('/', function () {return view('auth.login');});
+    //return Route::get('/', 'Auth\LoginController@showLoginForm');
+//});
+
+
+
+//Route::get('/', function () { return view('welcome'); });
+
+//Route::get('/home', 'HomeController@index')->name('home');
+
+//Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
